345d4fb7bfafebb13a109e804c2fea4b69c27c96
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-03-26 17:44:46 +0300

    Edit User using Put Mapping

--- src/main/java/kz/zhelezyaka/fullstackexperiment/controller/UserController.java
+++ src/main/java/kz/zhelezyaka/fullstackexperiment/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +35,16 @@ public class UserController {
     User getUserById(@PathVariable Long id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setFirstName(newUser.getFirstName());
+                    user.setLastName(newUser.getLastName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
     }
 }
